d58df7221076011eebea75900282228c7f45445a
Ali <soyseyitali23@gmail.com>
2023-03-05 18:48:37 +0300

    Entity Framework sonrası

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,16 +3,19 @@ using DataAccess.Abstract;
 using Entities;
 using DataAccess.Concrete.InMemory;
 using System;
+using DataAccess.Concrete.EntityFramework;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //Open Closed Principle
     internal class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetByUnitPrice(50,100))
             {
                 Console.WriteLine(product.ProductName);
             }
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -14,11 +15,11 @@ namespace DataAccess.Concrete.InMemory
         {
             _products = new List<Product>
             {
-                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitInStock=20,UnitPrice=5},
-                new Product{ProductId=2,CategoryId=3,ProductName="Kamera",UnitInStock=10,UnitPrice=5000},
-                new Product{ProductId=3,CategoryId=2,ProductName="Klavye",UnitInStock=30,UnitPrice=50},
-                new Product{ProductId=4,CategoryId=2,ProductName="Telefon",UnitInStock=213,UnitPrice=50000},
-                new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitInStock=20,UnitPrice=52},
+                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitsInStock=20,UnitPrice=5},
+                new Product{ProductId=2,CategoryId=3,ProductName="Kamera",UnitsInStock=10,UnitPrice=5000},
+                new Product{ProductId=3,CategoryId=2,ProductName="Klavye",UnitsInStock=30,UnitPrice=50},
+                new Product{ProductId=4,CategoryId=2,ProductName="Telefon",UnitsInStock=213,UnitPrice=50000},
+                new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitsInStock=20,UnitPrice=52},
 
 
             };
@@ -34,9 +35,19 @@ namespace DataAccess.Concrete.InMemory
             _products.Remove(productToDelete);
         }
 
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Product> GetAll()
         {
             return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Product> GetAllByCategory(int categoryId)
@@ -51,7 +62,7 @@ namespace DataAccess.Concrete.InMemory
             productToUpdate.CategoryId=product.CategoryId;
             productToUpdate.ProductName=product.ProductName;
             productToUpdate.UnitPrice=product.UnitPrice;
-            productToUpdate.UnitInStock=product.UnitInStock;
+            productToUpdate.UnitsInStock=product.UnitsInStock;
         }
     }
 }
