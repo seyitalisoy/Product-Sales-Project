3894ab8441bae3b9696df7950b3435d733753402
Ali <soyseyitali23@gmail.com>
2023-03-27 00:20:03 +0300

    Results yapısı oluşturuldu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,6 +4,7 @@ using Entities;
 using DataAccess.Concrete.InMemory;
 using System;
 using DataAccess.Concrete.EntityFramework;
+using Business.Constants;
 
 namespace ConsoleUI
 {
@@ -33,10 +34,20 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+           
         }
     }
 }
