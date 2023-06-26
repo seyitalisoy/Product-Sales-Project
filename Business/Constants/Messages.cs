552eb0e51373612c195d2277e8315196c55e11a1
Ali <soyseyitali23@gmail.com>
2023-06-26 17:21:14 +0300

    New Business Rules

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,10 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Urun ismi gecersiz";
         public static string MaintenanceTime = "Sistem bakimda";
         public static string ProductListed = "Urunler listelendi";
+        public static string ProductUpdated = "Product Updated";
+        public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir. ";
+        public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceded="Kategori limiti aşıldı.";
+        
     }
 }
